8164b53da5e402c350590f4f6fcbaae086c60fe2
SerenaMalusa <sere.malu@gmail.com>
2024-01-12 17:35:44 +0100

    feat(dadi): add all code

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -15,5 +15,29 @@ rollBtn.addEventListener('click', function() {
     // 3. generare numero random da 1 a 6 e stamparlo in pagina
     const pcNum = Math.floor( (Math.random() * 5) + 1 );
     pcNumOutput.innerText = pcNum;
+
+    // 4. creare un messaggio vuoto
+    let message = `parità!
+                    hanno vinto tutti
+                    o 
+                    non ha vinto nessuno?
+                    `
+
+    // 5. se il numero dell'utente è maggiore del numero random
+    if ( userNum > pcNum ) {
+
+        // messaggio: ha vinto l'utente
+        message = 'Hai vinto!!';
+    
+        // 7. se il numero dell'utente è minore del numero random
+    } else if (userNum < pcNum) {
+
+        // messaggio: ha vinto il computer
+        message = 'Hai perso :(';
+
+    }
+
+    // 8. stampare il messaggio
+    resultMessage.innerText = message;
     
 })
